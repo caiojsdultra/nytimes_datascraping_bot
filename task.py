c2c2d9c04972391349ac6808d890b0469f8a0b22
caiojsdultra <caiojsdultra@gmail.com>
2023-05-27 23:28:22 -0300

    1- change input method 2- change search phrase count

--- task.py
+++ task.py
@@ -19,36 +19,43 @@ output_fullpath = ''.join([configs['output_folder'],'/',configs['output_file_nam
 print('DATAFRAME:', return_dataframe)
 print(f'PARAMS: {search_new}, {date_range}, {sections}')
 
-#                                Search News
-steps.search_news()
-print(f'STEP 1 - Search News')
+try:
+    #                                Search News
+    steps.search_news()
+    print(f'STEP 1 - Search News')
 
-#                                Apply date Filters
-steps.apply_date_filters()
-print('STEPS 2 - Apply Date Filters')
-
-
-#                                Manage Search Results
-steps.apply_section_filters()
-print('STEPS 3 - Apply Section Filters')
-
-#                                Apply Section Filters
-search_count = steps.manage_search_results()
-print('STEPS 3 - Apply Section Filters')
+    #                                Apply date Filters
+    steps.apply_date_filters()
+    print('STEPS 2 - Apply Date Filters')
 
 
-#                                Workflow management
+    #                                Manage Search Results
+    steps.apply_section_filters()
+    print('STEPS 3 - Apply Section Filters')
 
-if search_count == '0':
-    steps.finish_process()
-    print(f"Process is finishing cause no search {configs['search']} results were found")
-else:
+    #                                Apply Section Filters
+    search_count = steps.manage_search_results()
+    print('STEPS 3 - Apply Section Filters')
 
-    #                               Getting news information
-    steps.click_more_button()
-    new_df = steps.get_news_information()
-    #                               Write Excel File
-    utils.write_excel_file(new_df, output_fullpath)
-    print('Using this DF to create excel file:', new_df)
+
+    #                                Workflow management
+
+    if search_count == '0':
+        steps.finish_process()
+        print(f"Process is finishing cause no search {configs['search']} results were found")
+    else:
+
+        #                               Getting news information
+        steps.click_more_button()
+        new_df = steps.get_news_information()
+        #                               Write Excel File
+        utils.write_excel_file(new_df, output_fullpath)
+        print('Using this DF to create excel file:', new_df)
+except Exception as e:
+
+    if len(new_df) > 0:
+        utils.write_excel_file(new_df, output_fullpath)
+
+    print(f'An error occurred while executing the process: {e}')
 
 
